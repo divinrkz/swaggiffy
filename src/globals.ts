1cc29c1cccf32a080d8bfe39fd626a8b13424e7d
Divin Irakiza <irakizadivin@gmail.com>
2021-10-09 17:00:23 +0200

    [chore]: improve package.json

--- src/globals.ts
+++ src/globals.ts
@@ -12,8 +12,6 @@ import { ConfigurationProps } from './typings';
 
 declare global {
     namespace Express {
-        // These open interfaces may be extended in an application-specific manner via declaration merging.
-        // See for example method-override.d.ts (https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/method-override/index.d.ts)
         interface Request {}
         interface Response {}
         interface Application {
