323dbece8ecca616da445071f7de1aed1d3c8f53
DivinIrakiza <irakizadivin@gmail.com>
2021-12-06 18:17:33 +0200

    (chore): setup app

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,19 @@
-import express from 'express';
+import * as express from 'express';
 
+
+class App {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+    }
+
+    public listen() {
+        this.app.listen(4008, () => {
+            console.log(`App listening on the port ${process.env.PORT}`);
+        })
+    }  
+};
+
+export default App;
