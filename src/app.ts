af7215cb16b9360357c6c4042f9576ba5e3c78c9
Divin Irakiza <irakizadivin@gmail.com>
2021-12-07 12:21:02 +0200

    (chore): express application return server

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,33 @@
-import express from 'express';
+import express, {Express} from 'express';
 import Config from './config';
 
 class App extends Config {
 
     private PORT: number = parseInt(process.env.PORT as string);
 
-    public app: express.Application;
+    public app: Express;
 
     constructor() {
         super();
         this.app = express();
+        this.serveRoutes();
     }
 
     public listen() {
-        console.log('Running');
-    
         this.app.listen(this.PORT, () => {
             console.log(`App listening on the port ${process.env.PORT}`);
         })
     }  
+    
 
-    public getServer(): express.Application {
+    public getServer(): express {
         return this.app;
+    }
+
+    public serveRoutes() {
+        this.app.get('/', (req, res) => {
+            res.status(200).send(`Server made up and running!`)
+        });
     }
 
 };
---
+++ src/config/route.config.ts
@@ -0,0 +1,6 @@
+import {Express} from 'express';
+
+
+export abstract class RoutesConfig {
+    private _app: Express
+}
