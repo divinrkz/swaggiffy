4b77f65363b48910841bc358a1af66ba2f29608b
Divin Irakiza <irakizadivin@gmail.com>
2021-12-20 10:05:06 +0200

    chore: Swaggify methods typed

--- src/lib/SwaggifyModel.ts
+++ src/lib/SwaggifyModel.ts
@@ -5,7 +5,6 @@ function SwaggifyModel(): Function {
   return (_class: Function) => {
 
       const classDef: TClassDef = Utility.getClassProps(_class);    
-
       const swaggerDef: TSwaggerSchema = Utility.genDef(classDef);
 
       Utility.swaggify(swaggerDef);
