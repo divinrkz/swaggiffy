a2b26338ac5c3fd5190804e3ac6b060d28ee4839
Divin Irakiza <irakizadivin@gmail.com>
2021-12-30 17:57:04 +0200

    [chore]: storing schemas

--- src/lib/decorators/swaggify-model.ts
+++ src/lib/decorators/swaggify-model.ts
@@ -2,23 +2,19 @@ import { PlatformTools } from "../platform/PlatformTools";
 import { TClassDef, TSwaggerSchema } from "../../typings";
 import {Utility} from "../../utils/Utility";
 import {getSchemaMetadataStorage} from '../../globals';
+import {ISchemaMetaData} from '../../storage/types/ISchemaMetaData';
 
-export const SwaggifyModel = (): Function => {
+export const SwaggifyModel = (): ClassDecorator => {
   return (target: Function) => {
 
-
-
-
       const classDef: TClassDef = Utility.getClassProps(target);    
-      const swaggerDef: TSwaggerSchema = Utility.genSchemaDef(classDef);
+      const swaggerDefinition: TSwaggerSchema = Utility.genSchemaDef(classDef);
 
-      // console.log('swagger Definiction', swaggerDef);
-
-      // PlatformTools.getGlobalVariable();
-      const data = getSchemaMetadataStorage().schemas.push(swaggerDef);
-  
-      console.log(getSchemaMetadataStorage());
-      // Utility.swaggify(swaggerDef);
+      getSchemaMetadataStorage().schemas.push({
+        target,
+        name: classDef.class,
+        swaggerDefinition
+      } as ISchemaMetaData);
   }
 }
 
--- src/storage/schema-metadata-storage.ts
+++ src/storage/schema-metadata-storage.ts
@@ -1,14 +1,14 @@
 import { TSwaggerSchema } from "../typings";
+import {ISchemaMetaData} from './types/ISchemaMetaData';
+
 
 /**
  * Global storage for registered schemas
  */
 export class SchemaMetadataStorage {
-
     /**
      * Swagger schemas
      */
-    readonly schemas: TSwaggerSchema[] = [];
-
-
+    readonly schemas: ISchemaMetaData[] = [];
 }
+
