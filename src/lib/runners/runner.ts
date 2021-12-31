5afcd0d46c2bc8498789a52a4beb36eb7e35e1a4
Divin Irakiza <irakizadivin@gmail.com>
2021-12-31 10:54:18 +0200

    [chore]: generating swagger

--- src/lib/runners/runner.ts
+++ src/lib/runners/runner.ts
@@ -1,7 +1,17 @@
-export class Runner {
-    generateSchemas(): Promise<string> {
-        return new Promise<string> ((resolve, reject) => {
+import { SchemaMetadata } from '../../storage/types/SchemaMetadata';
+import { getSchemaMetadataStorage } from '../../globals';
 
-        })
+
+/**
+ * Runner Class
+ */
+export class Runner {
+    
+    /**
+     * Generating schemas from global SchemaMetadataStorage
+     * @param schemas 
+     */
+    generateSchemas(): void {
+        console.log(getSchemaMetadataStorage().schemas);
     }
 }
