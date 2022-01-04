a667ad2dbed3e6df8c9d103d831b93c285adf5d3
Divin Irakiza <irakizadivin@gmail.com>
2022-01-04 11:52:18 +0200

    [chore]: added OASv3.0 types and formats

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -4,17 +4,14 @@ export type Class<T> = {new (): T};
  * The OpenSpecification 3.0 types
  */
 const TDataType = 'integer' | 'number' | 'string' | 'boolean';
-
 /**
  * The OpenSpecification 3.0 integer type formats
  */
 const TIntegerFormat = 'int32' | 'int64';
-
 /**
  * The OpenSpecification 3.0 number type formats
  */
 const TNumberFormat = 'float' | 'double';
-
 /**
  * The OpenSpecification 3.0 string type formats
  */
