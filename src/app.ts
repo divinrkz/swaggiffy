c11c53e7d366ea7eff2a0ace1be742b6686fdb67
Divin Irakiza <irakizadivin@gmail.com>
2022-01-04 13:13:21 +0200

    [chore]: added OASv3.0 types and formats

--- src/app.ts
+++ src/app.ts
@@ -35,23 +35,18 @@ class App extends Config {
         });      
     }
 
-    private swaggify(): void {    
+    private async swaggify() {    
 
         new Phone();
         new Person();
         new User();
         
-        
-        Runner.generateSchemas();
+        Runner.execute();
 
-        setTimeout(() => {
-            import('./swagger/swagger.json').then((file: any) =>  {
-                const specs: JsonObject = swaggerJsdoc(file);
-                this.app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
-            }
-        );
-        }, 2000);
-
+        import('./swagger/swagger.json').then((file: any) =>  {
+            const specs: JsonObject = swaggerJsdoc(file);
+            this.app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
+        }
     }
 };
 
--- src/lib/runners/runner.ts
+++ src/lib/runners/runner.ts
@@ -13,10 +13,12 @@ export class Runner {
      * Generating schemas from global SchemaMetadataStorage
      * @param schemas 
      */
-    static generateSchemas(): void {
-
+    static generateSchemas():void {
         const definition: TSwaggerSchemaDef = Utility.compressArrToObj(getSchemaMetadataStorage().schemas);
-        Utility.swaggify(definition);
+        Utility.swaggify(definition)
+    }
 
+    static execute(): void {
+        this.generateSchemas();
     }
 }
