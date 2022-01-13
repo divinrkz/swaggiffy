46f15dcfed5a15448bcb81dbac72c9da24520490
Divin Irakiza <irakizadivin@gmail.com>
2022-01-13 15:11:56 +0200

    [feat]: production ready codebase

--- src/errors/SwaggifyError.ts
+++ src/errors/SwaggifyError.ts
@@ -1,7 +1,10 @@
 export class SwaggifyError extends Error {
-  constructor(message:string = 'An unexpected error occurred', code:string='SWAGGIFY_ERROR') {
-    super(message);
-    this.name = code;
+  constructor(
+    message: string = 'An unexpected error occurred',
+    code: string = 'SWAGGIFY_ERROR'
+  ) {
+    super(message)
+    this.name = code
     Error.captureStackTrace(this, SwaggifyError)
   }
 }
