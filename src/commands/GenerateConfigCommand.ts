5c354bca25027cf8ce711c9861176c02cfbb43e6
Divin Irakiza <irakizadivin@gmail.com>
2022-01-22 08:48:00 +0200

    feat: add refresh generated config file

--- src/commands/GenerateConfigCommand.ts
+++ src/commands/GenerateConfigCommand.ts
@@ -12,18 +12,24 @@ export class GenerateConfigCommand implements yargs.CommandModule {
     aliases = "g:config";
 
     builder(args: yargs.Argv) {
-        return args.option("p", {
-            alias: "configFile",
+        return args.option("path", {
+            alias: "configFilePath",
             type: "string",
             describe: "File where the config file should be created. Defaults to BASE_DIR/swaggify.config.json .",
+        }).option("r", {
+            alias: "refresh",
+            type: "boolean",
+            describe: "Re-generate and overwrite existing config file.",
         });
     }
 
     async handler(args: yargs.Arguments) {
         try {
-            const configFile: string = await SetupRunner.generateConfigFile(GenerateConfigCommand.getTemplate(), args.configFile as string | undefined);
+            const override: boolean | undefined = (args.refresh) ? true : false;
+            const configFile: string = await SetupRunner.generateConfigFile(GenerateConfigCommand.getTemplate(), args.configFilePath as string | undefined, override as boolean);
             console.log(`Created: ${FileUtils.cleanPath(configFile)}`);
             PlatformTools.logSuccess("Successfully generated");
+
         } catch (err: any) {
             PlatformTools.logCmdErr("Error when generating config file: ", err);
         }
--- src/runners/SetupRunner.ts
+++ src/runners/SetupRunner.ts
@@ -4,8 +4,8 @@ export class SetupRunner {
     /**
      * Generate Swaggify Configuration File Template
      */
-    static async generateConfigFile(template: string, configFile?: string): Promise<string> {
-        const filePath: string = await FileUtils.createFileInWorkspace(configFile || Defaults.SWAGGIFY_CONFIG_FILE);
+    static async generateConfigFile(template: string, configFile?: string, override?: boolean): Promise<string> {
+        const filePath: string = await FileUtils.createFileInWorkspace(configFile || Defaults.SWAGGIFY_CONFIG_FILE, override);
         return await FileUtils.writeToFile(filePath, template);
     }
 }
