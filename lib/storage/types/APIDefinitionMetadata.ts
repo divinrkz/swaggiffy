a74636613f508405c7c144d1958636a7292a0990
divinirakiza <irakizadivin@gmail.com>
2022-02-07 22:47:25 +0200

    feat: rename createDefinition to regiisterDefinition

--- lib/storage/types/APIDefinitionMetadata.ts
+++ lib/storage/types/APIDefinitionMetadata.ts
@@ -1,10 +1,11 @@
 import { APIPathDefinition, TSwaggerSchema } from '../../typings';
+import * as express from 'express';
 
 export interface APIDefinitionMetadata {
     /**
      * Router which owns the API Definition
      */
-    readonly router: any;
+    readonly router: express.Router;
 
     /**
      * Swagger Definition for the schema
