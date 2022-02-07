bf23b0500c73a16c8c5e92b566fce41d66bed74a
divinirakiza <irakizadivin@gmail.com>
2022-02-07 23:00:33 +0200

    feat: changes

--- lib/index.ts
+++ lib/index.ts
@@ -41,7 +41,6 @@ router.delete('/:id', (req: any, res: any) => {
     res.send('Delete');
 });
 
-console.log(router);
 
-registerDefinition({ router });
+registerDefinition({ router, tags: 'Users' });
 new Swaggify().setupExpress(app).swaggify();
