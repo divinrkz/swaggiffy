8afa15bbb992aaa5e3a1e751c4646a56a45cea74
Divin Irakiza <irakizadivin@gmail.com>
2022-04-05 15:46:23 +0200

    [chore]: add reflecct-metadata

--- src/globals.ts
+++ src/globals.ts
@@ -3,6 +3,28 @@ import { SchemaMetadataStorage } from './storage/schema-metadata-storage';
 import { ConfigMetadataStorage } from './storage/ConfigMetadataStorage';
 import { ConfigurationProps } from './typings';
 
+
+/**
+ * Global string method reversed for paths
+ */
+ Object.defineProperty( String.prototype, 'getAbsolutePath', {
+	value: function () {
+		process.cwd() + '/' + this;
+	}
+} )
+
+
+declare global {
+    namespace Express {
+        // These open interfaces may be extended in an application-specific manner via declaration merging.
+        // See for example method-override.d.ts (https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/method-override/index.d.ts)
+        interface Request {}
+        interface Response {}
+        interface Application {}
+    }
+}
+
+
 /**
  * Returns globals schemametadata storage
  */
@@ -20,6 +42,7 @@ export function getConfigMetadataStorage(): ConfigMetadataStorage {
     const globalScope = PlatformTools.getGlobalVariable();
     if (!globalScope.configMetadataStorage) globalScope.configMetadataStorage = new ConfigMetadataStorage();
 
+    globalScope.configMetadataStorage.swaggerDefinitionFilePath.getAbsolutePath();
     return globalScope.configMetadataStorage;
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
+import 'reflect-metdata';
+
+export * from "./globals";
+export * from './cli';
 export * from "./decorators/Schema";
 export * from "./Swaggify";
-export * from './cli';
-// import { Swaggify } from './Swaggify';
-// import express, { Express } from 'express';
 
-// // SetupRunner.generateConfigFile();
-
-// const expressApp: Express = express();
-
-// new Swaggify().setupExpress(expressApp).swaggify();
-
-// InitRunner.extractConfigurations();
